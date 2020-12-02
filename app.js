4d5b5cacad4ae60560b7f87c978ce166351604a7
Akhil Konduru <60151778+maverick1910@users.noreply.github.com>
2020-12-02 18:16:21 +0530

    Version 2

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+// Packages config
 var express=require("express")
 const app=express()
 var bodyparser=require("body-parser")
+var methodOverride=require("method-override")
 //APP CONFIG
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
-// Model COnfig
+// Model Config
 var mongoose=require("mongoose")
 mongoose.connect("mongodb://localhost/site_app");
+
 //Model Schema 
 var siteSchema =mongoose.Schema({
     title:String,
@@ -16,12 +20,14 @@ var siteSchema =mongoose.Schema({
     body:String,
     date:{type:Date,default:Date.now}
 });
-var Site =mongoose.model("Site",siteSchema);
+var Site =mongoose.model("Site",siteSchema); // variable created for easy accessing 
+
 // Routes 
 app.get('/',function(req,res){
     res.redirect('site');
 });
 
+// display all blogs on the site
 app.get('/site',function(req,res){
     Site.find({},function(err,sites){
         if(err)
@@ -31,7 +37,7 @@ app.get('/site',function(req,res){
         else{
             res.render('site',{site:sites});
         }
-    })
+    });
 });
 
 //Create new Blog
@@ -48,6 +54,51 @@ app.post('/site',function(req,res){
         }
         else{
             res.redirect('site')
+        }
+    });
+});
+
+//Show Blog
+app.get('/site/:id',function(req,res){
+    Site.findById(req.params.id,function(err,foundSite){
+        if(err)
+        {
+            res.redirect('/site')
+        }else{
+            res.render('show',{site:foundSite});
+        }
+    });
+});
+
+//Edit Route
+app.get('/site/:id/edit',function(req,res){
+    Site.findById(req.params.id,function(err,foundSite){
+        if(err){
+            res.redirect('/site')
+        }else{
+            res.render('edit',{site:foundSite});
+        }
+    });
+});
+
+//Update Route
+app.put('/site/:id',function(req,res){
+    Site.findByIdAndUpdate(req.params.id,req.body.site,function(err,updatedSite){
+        if(err){
+            res.redirect('/site')
+        }else{
+            res.redirect('/site/'+ req.params.id);
+        }
+    });
+});
+
+//Delete Route
+app.delete('/site/:id',function(req,res){
+    Site.findByIdAndRemove(req.params.id,function(err){
+        if(err){
+            res.redirect('/site')
+        }else{
+            res.redirect('/site')
         }
     });
 });
